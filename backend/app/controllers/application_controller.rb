a368738a26597698812637c938c69bf4493f1092
abaez8031 <abaez8031@gmail.com>
2024-06-12 00:38:43 -0400

    auth helpers

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,6 +1,29 @@
 class ApplicationController < ActionController::API
   before_action :snake_case_params
 
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])# user whose `session_token` == token in `session` cookie
+  end
+  
+  def login!(user)
+    # reset `user`'s `session_token` and store in `session` cookie
+    session[:session_token] = user.reset_session_token!
+  end
+  
+  def logout!
+    # reset the `current_user`'s session cookie, if one exists
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    # clear out token from `session` cookie
+    @current_user = nil # so that subsequent calls to `current_user` return nil
+  end
+  
+  def require_logged_in
+    unless current_user
+      render json: { message: 'Unauthorized' }, status: :unauthorized 
+    end
+  end
+  
   private
 
   def snake_case_params
