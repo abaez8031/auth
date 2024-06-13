bb641f96633c492094875a34cfd84a8a6d846fd7
abaez8031 <abaez8031@gmail.com>
2024-06-13 05:30:38 -0400

    loginformpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
+import { Switch, Route, Link} from "react-router-dom"
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route exact path="/login">
+        <LoginFormPage/>
+      </Route>
+    </Switch>
   );
 }
 
--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,5 +1,43 @@
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { login } from "../../store/session";
+import { Redirect } from "react-router-dom"
+
 const LoginFormPage = () => {
-  return
+  const currentUser = useSelector(state => state.session.user)
+  console.log(currentUser)
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      credential, password
+    }
+    dispatch(login(user))
+  }
+
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
+  
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="credential">Email or password</label>
+      <input type="text" id="credential" value={credential} onChange={(e) => {
+        setCredential(e.target.value)
+      }}>
+      </input>
+
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" value={password} onChange={(e) => {
+        setPassword(e.target.value)
+      }}></input>
+      <button>Submit</button>
+    </form>
+    </>
+  )
 }
 
 export default LoginFormPage;
